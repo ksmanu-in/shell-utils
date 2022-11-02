456a208a775fccefd0229e308cf6b4387f3b7ece
Kumar Sambhav <ksmanu.in@gmail.com>
2022-11-03 01:25:07 +0530

    fixed octal representation causing value too great for base error

--- clock.sh
+++ clock.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 #Runs a clock, and keeps checking and when hits the given time passed as 1st argument rings a bell.
+
+MATCH_WITH=$1
+MATCH_WITH=${MATCH_WITH#0}
 while true
   do 
 	  sleep 10 
 	  DT=$(date +%H%M )
+	  DTO=$DT
+	  DT=${DT#0}
 	  tput cup 24 0
-	  echo -n $DT
+	  echo -n $DTO
 
-	  if (($DT == $1)) 
+	  if (($DT >= $MATCH_WITH)) 
 	  then
-	    echo ^G^G^G^G^G Its time
+	    echo -n  Its time
 	    break
 	  fi
   done
